b6cdabed45da838b45b29fc4a45a19192a341479
Austin Rivera <austin.rivera.24@aol.com>
2023-12-19 05:34:13 -0500

    Dodger movement

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code here
+// Move Left
+const dodger = document.getElementById('dodger');
+const gameWidth = window.innerWidth;
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+  // Move Right
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    const dodgerWidth = dodger.offsetWidth;
+  
+    if (left < gameWidth - dodgerWidth) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+  
